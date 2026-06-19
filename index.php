22635ac75a32d9aa3bd3a49f7a64ae9dd0ca96ef
Yimy.Romero <yimy.romero80@gmail.com>
2026-06-18 20:15:18 -0400

    fix: remove index.php from gitignore.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+get_header();
+?>
+<main id="site-content">
+    <?php
+    if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
+            get_template_part( 'template-parts/content/content', get_post_type() );
+        endwhile;
+        the_posts_navigation();
+    else :
+        get_template_part( 'template-parts/content/content', 'none' );
+    endif;
+    ?>
+</main>
+<?php
+get_sidebar();
+get_footer();
