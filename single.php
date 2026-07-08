a00051f239f9d914e4eb36c5c00401a02e3fddfb
Yimy.Romero <yimy.romero80@gmail.com>
2026-07-08 00:05:30 -0400

    fix: remove container class from main div wrappers.

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php
 get_header();
 ?>
-<main id="site-content" class="container">
+<main id="site-content" class="">
     <?php
     if ( have_posts() ) :
         while ( have_posts() ) : the_post();
